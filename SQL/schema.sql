1c3cb9d131b9394a9235249d0a1ae16723bff0f5
Hack Reactor Students <rob@robertwholmes.com>
2014-03-21 15:45:38 -0700

    create SQL table schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,13 +1,69 @@
+DROP DATABASE IF EXISTS `chat`;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
- /* Describe your table here.*/
-);
-
-/* You can also create more tables, if you need them... */
-
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
+
+ -- Globals
+ -- ---
+
+ -- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+ -- SET FOREIGN_KEY_CHECKS=0;
+
+ -- Table 'users'
+
+ DROP TABLE IF EXISTS `users`;
+
+ CREATE TABLE `users` (
+   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+   `username` VARCHAR(20) NULL DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ );
+
+ -- Table 'messages'
+
+ DROP TABLE IF EXISTS `messages`;
+
+ CREATE TABLE `messages` (
+   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+   `body` VARCHAR(160) NULL DEFAULT NULL,
+   `user_id` INTEGER NULL DEFAULT NULL,
+   `send_time` DATETIME NULL DEFAULT NULL,
+   `room_id` INTEGER NULL DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ );
+
+ -- Table 'rooms'
+
+ DROP TABLE IF EXISTS `rooms`;
+
+ CREATE TABLE `rooms` (
+   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+   `name` VARCHAR(15) NULL DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ );
+
+ -- Foreign Keys
+
+ ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ ALTER TABLE `messages` ADD FOREIGN KEY (room_id) REFERENCES `rooms` (`id`);
+
+ -- Table Properties
+
+ -- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+ -- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+ -- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+ -- Test Data
+
+ -- INSERT INTO `users` (`id`,`username`) VALUES
+ -- ('','');
+ -- INSERT INTO `messages` (`id`,`body`,`user_id`,`send_time`,`room_id`) VALUES
+ -- ('','','','','');
+ -- INSERT INTO `rooms` (`id`,`name`) VALUES
+ -- ('','');
+
